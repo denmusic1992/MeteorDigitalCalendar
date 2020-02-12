b3dfd87e6160a6da954ab1b60bd6461fa7e92665
denis <denmusic92@mail.ru>
2020-02-12 13:05:49 +0700

    Работает в xCode;

--- app/src/iosMain/kotlin/network/CoroutineDispatchers.kt
+++ app/src/iosMain/kotlin/network/CoroutineDispatchers.kt
@@ -2,6 +2,9 @@ package network
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Runnable
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
+import platform.darwin.dispatch_queue_t
 import kotlin.coroutines.CoroutineContext
 
 actual object CoroutineDispatchers {
--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -1,14 +1,35 @@
 import UIKit
 import app
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, RegistrationViewInterface {
+    func authorizationResult(success: Bool, reason: String?) {
+        
+    }
+    
+    func registerResult(success: Bool, reason: String?) {
+        print(reason ?? "все ок!")
+    }
+    
+    func registrationResult(success: Bool, errors: NSMutableArray?, messageError: String?) {
+        
+    }
+    
+    func resendEmailResult(success: Bool, message: String?) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         label.text = Proxy().proxyHello()
+        let settings: Settings = AppleSettings(delegate: UserDefaults.standard)
+        registrationPresenter = RegistrationPresenterImpl(registrationView: self, settings: settings)
+        
+        registrationPresenter?.registerDevice()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     @IBOutlet weak var label: UILabel!
+    private var registrationPresenter: RegistrationPresenter?
 }
